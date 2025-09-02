1da64cc338b33ba9995cab6c5d473f705453da74
LenryHouie <henry3.louie@gmail.com>
2025-09-01 21:08:07 -0700

    Update classroom.js

--- classroom.js
+++ classroom.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 import {
   getFirestore,
   doc,
